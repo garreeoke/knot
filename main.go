32e13f601444d26795c761d75f10efd29f6637f9
Aaron Torgerson <torgerson.aaron@gmail.com>
2019-03-29 18:28:45 -0700

    cleanup

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+
+	"log"
+	"os"
+	//"github.com/garreeoke/kates"
+)
+
 func main() {
+
+	// Get source at path for yamls
+	sourceType := os.Getenv("KNOT_SOURCE_TYPE")
+	sourcePath := os.Getenv("KNOT_SOURCE_PATH")
+
+	if sourceType == "" {
+		log.Fatalln("No source type given")
+	}
+
 
 
 }
